76648f99eb318c09703b2419e5ded240611b992d
choiway <waynechoi@gmail.com>
2018-06-27 07:38:07 -0700

    fix config

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,5 @@
 pub static TARGET_TICKER: &str = "krakenUSD";
 
-// Mutate probably of dna applied after crossover
-pub static MUTATE_PROB: f32 = 0.20;
-
 // The number of fittest chromosomes to evolve
 pub static FITTEST: usize = 500;
 
